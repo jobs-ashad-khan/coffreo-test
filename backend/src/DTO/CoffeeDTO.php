e2685ea1917bc79105ce76cd7c3aa29356183cde
Khan Ashad <jobs.ashad.khan@gmail.com>
2025-03-12 17:42:16 +0100

    [Backend]: Fix CoffeeDTO

--- backend/src/DTO/CoffeeDTO.php
+++ backend/src/DTO/CoffeeDTO.php
@@ -5,7 +5,6 @@ namespace App\DTO;
 class CoffeeDTO
 {
     public function __construct(
-        public string $id,
         public string $type,
         public int $intensity,
         public string $size
