7a2a290d0c9e231a66be397fdecef9770d89df93
Khan Ashad <jobs.ashad.khan@gmail.com>
2025-03-12 12:47:02 +0100

    [Frontend]: Add coffeeOrderService to fetch API data

---
+++ frontend/utils/coffeeOrderService.tsx
@@ -0,0 +1,18 @@
+import { CoffeeDTO } from "@/types/CoffeeDTO";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+/**
+ * Envoie d'une commande de café à l'API Symfony
+ */
+export const createCoffeeOrder = async (coffee: CoffeeDTO): Promise<void> => {
+    const response = await fetch(`${API_URL}/coffee-orders`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(coffee),
+    });
+
+    if (!response.ok) {
+        throw new Error("Erreur lors de la création de la commande");
+    }
+};
